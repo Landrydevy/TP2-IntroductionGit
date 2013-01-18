ac884e9094e8ffe2bccbc13ea348f4f21f739cdd
Naa <nicolas.lemaire91240@gmail.com>
2013-01-18 11:48:38 +0100

    3

--- TP1-Banque/src/Client.java
+++ TP1-Banque/src/Client.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 
 public class Client {
 	private String nom, prenom;
-	private ArrayList<Compte> lesComptes = new ArrayList<Compte>();;
-	private ArrayList<Client> toutLesClients = new ArrayList<Client>();
+	private ArrayList<Compte> lesComptes = new ArrayList<Compte>();
 	
 	public Client(String unNom, String unPrenom){
 		this.nom = unNom;
@@ -12,11 +11,35 @@ public class Client {
 	}
 	
 	//retourne le solde général du client (tous comptes pris en compte)
-	public double soldeClient(){
+	public double soldeClient(Client unClient){
 		
 		return (2);
 	}
 	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public ArrayList<Compte> getLesComptes() {
+		return lesComptes;
+	}
+
+	public void setLesComptes(ArrayList<Compte> lesComptes) {
+		this.lesComptes = lesComptes;
+	}
+
 	//retourne un ensemble de clients qui ont au moins un compte dans le rouge
 	public ArrayList<Client> compteADecouvert(){
 		
